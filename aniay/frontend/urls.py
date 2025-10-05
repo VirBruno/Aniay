3504a82822ab6a63473edd7fcbfb799b1b48e3c2
Vir Bruno <virbruno27@gmail.com>
2025-10-05 17:33:20 -0300

    Cambio backend Flash

---
+++ aniay/frontend/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", view=views.home, name="home"),
+    path("proveedores/", view=views.proveedores_repository, name="proveedores_repo"),
+    path("proveedores/nuevo/", view=views.proveedores_form, name="proveedores_form"),
+    path("proveedores/editar/<int:id>/", view=views.proveedores_form, name="proveedores_edit"),
+    path("proveedores/eliminar/", view=views.proveedores_delete, name="proveedores_delete"),
+    path("juguetes/", view=views.juguetes_repository, name="juguetes_repo"),
+    path("juguetes/nuevo/", view=views.juguetes_form, name="juguetes_form"),
+    path("juguetes/editar/<int:id>/", view=views.juguetes_form, name="juguetes_edit"),
+    path("juguetes/eliminar/", view=views.juguetes_delete, name="juguetes_delete"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ aniay/frontend/views.py
@@ -0,0 +1,150 @@
+from datetime import date
+
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+
+from .models import Proveedor, Juguete
+
+import requests
+
+
+def home(request):
+    """
+    Vista para mostrar la página de inicio.
+    """
+    resp = requests.get("http://backend:5000/juguetes")
+    juguetes = resp.json()
+    return render(request, "home.html", {"juguetes": juguetes})    
+    """juguetes = Juguete.objects.all()  # o filtro si querés algunos específicos
+    return render(request, 'home.html', {'juguetes': juguetes}) CÓDIGO ANTERIOR"""
+
+
+def proveedores_repository(request):
+    """
+    Vista para mostrar el repositorio de proveedores.
+    """
+    proveedores = Proveedor.objects.all()
+    return render(request, "proveedores/repository.html", {"proveedores": proveedores})
+
+
+def proveedores_form(request, id=None):
+    """
+    Vista para mostrar y procesar el formulario de usuario.
+
+    Esta vista maneja la presentación y el procesamiento del formulario de proveedor.
+    Si se recibe una solicitud POST, la vista intenta guardar o actualizar el proveedor
+    según los datos recibidos. Si se recibe una solicitud GET, la vista muestra el formulario
+    para crear un nuevo proveedor o para actualizar uno existente, según el parámetro 'id'.
+    """
+    if request.method == "POST":
+        proveedor_id = request.POST.get("id", "")
+        errors = {}
+        saved = True
+
+        if proveedor_id == "":
+            saved, errors = Proveedor.save_proveedor(request.POST)
+        else:
+            proveedor = get_object_or_404(Proveedor, pk=proveedor_id)
+            saved, errors = proveedor.update_proveedor(request.POST)
+
+        if saved:
+            return redirect(reverse("proveedores_repo"))
+
+        return render(
+            request, "proveedores/form.html", {"errors": errors, "proveedor": request.POST},
+        )
+
+    proveedor = None
+    if id is not None:
+        proveedor = get_object_or_404(Proveedor, pk=id)
+
+    return render(request, "proveedores/form.html", {"proveedor": proveedor})
+
+
+def proveedores_delete(request):
+    """
+    Vista para eliminar un proveedor.
+
+    Esta vista elimina un proveedor de la base de datos según el ID proporcionado en la solicitud POST.
+    """
+    proveedor_id = request.POST.get("proveedor_id")
+    proveedor = get_object_or_404(Proveedor, pk=int(proveedor_id))
+    proveedor.delete()
+
+    return redirect(reverse("proveedores_repo"))
+
+def juguetes_repository(request):
+    """Vista para mostrar el repositorio de juguetes.
+
+    Esta vista obtiene todas las jugutes de la base de datos y las pasa a la plantilla
+    'juguetes/repository.html' para su renderizado.
+    """
+    juguetes = Juguete.objects.all()
+    return render(request, "juguetes/repository.html", {"juguetes": juguetes})
+
+def juguetes_form(request, id=None):
+    today = date.today().strftime('%Y-%m-%d')
+    proveedores = Proveedor.objects.all()   # <-- faltaba
+
+    if request.method == "POST":
+        juguete_id = request.POST.get("id", "")
+        proveedor_id = request.POST.get("proveedor")
+        proveedor = get_object_or_404(Proveedor, pk=proveedor_id)
+        image = request.FILES.get("image")
+
+        if juguete_id == "":
+            juguete = Juguete.objects.create(
+                name=request.POST["name"],
+                brand=request.POST["brand"],
+                description=request.POST["description"],
+                price=request.POST["price"],
+                proveedor=proveedor,
+                image=request.FILES.get("image")
+                
+            )
+        else:
+            juguete = get_object_or_404(Juguete, pk=juguete_id)
+            juguete.name = request.POST["name"]
+            juguete.brand = request.POST["brand"]
+            juguete.description = request.POST["description"]
+            juguete.price = request.POST["price"]
+            juguete.proveedor = proveedor
+            juguete.image=request.FILES.get("image")
+
+        return redirect("juguetes_repo")
+
+    juguete = None
+    if id is not None:
+        juguete = get_object_or_404(Juguete, pk=id)
+
+    return render(request, "juguetes/form.html", {
+        "juguete": juguete,
+        "today": today,
+        "proveedores": proveedores,})
+
+    juguete = None
+    if id is not None:
+        juguete = get_object_or_404(Juguete, pk=id)
+        
+    return render(request, "juguetes/form.html", {"juguete": juguete, "today": today})
+
+def juguetes_delete(request):
+    """
+    Vista para eliminar una juguete.
+
+    Esta vista elimina una juguete de la base de datos según el ID proporcionado en la solicitud POST.
+    """
+    juguete_id = request.POST.get("juguete_id")
+    juguete = get_object_or_404(Juguete, pk=int(juguete_id))
+    juguete.delete()
+
+    return redirect(reverse("juguetes_repo"))
+
+def nuevo_juguete(request):
+    if request.method == 'POST':
+        form = JugueteForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_juguetes')
+    else:
+        form = JugueteForm()
+    return render(request, 'juguetes_form.html', {'form': form})
